ec3557d08c28e6e27d9a468f4f81530ba1f254de
송현서 <s22026@gsm.hs.kr>
2023-08-31 23:09:04 +0900

    Chain, Barrier

--- app/src/main/java/com/compose/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/compose/constraintlayout/MainActivity.kt
@@ -13,9 +13,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import com.compose.constraintlayout.ui.theme.ConstraintlayoutTheme
@@ -33,79 +33,47 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ConstraintlayoutExample() {
-    val constraintSet = ConstraintSet {
-        val redBox = createRefFor("redBox")
-        val magentaBox = createRefFor("magentaBox")
-        val greenBox = createRefFor("greenBox")
-        val yellowBox = createRefFor("yellowBox")
+    ConstraintLayout(Modifier.fillMaxSize()) {
+        val (redBox, yellowBox, magentaBox, text) = createRefs()
 
-        constrain(redBox) {
-            bottom.linkTo(parent.bottom, margin = 8.dp)
-            end.linkTo(parent.end, margin = 8.dp)
-        }
-
-        constrain(magentaBox) {
-            start.linkTo(parent.start)
-            end.linkTo(parent.end)
-        }
-
-        constrain(greenBox) {
-            centerTo(parent)
-        }
-
-        constrain(yellowBox) {
-            start.linkTo(magentaBox.end)
-            top.linkTo(magentaBox.bottom)
-        }
-    }
-
-    ConstraintLayout(
-        constraintSet,
-        modifier = Modifier.fillMaxSize()
-    ) {
         Box(
             modifier = Modifier
                 .size(40.dp)
                 .background(Color.Red)
-                .layoutId("redBox")
-//                .constrainAs(redBox) {
-//                    bottom.linkTo(parent.bottom, margin = 8.dp)
-//                    end.linkTo(parent.end, margin = 8.dp)
-//                }
+                .constrainAs(redBox) {
+                    start.linkTo(parent.start, margin = 10.dp)
+                }
         )
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(Color.Magenta)
-                .layoutId("magentaBox")
-//                .constrainAs(magentaBox) {
-//                    start.linkTo(parent.start)
-//                    end.linkTo(parent.end)
-//                }
-        )
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(Color.Green)
-                .layoutId("greenBox")
-//                .constrainAs(greenBox) {
-////                    start.linkTo(parent.start)
-////                    end.linkTo(parent.end)
-////                    top.linkTo(parent.top)
-////                    bottom.linkTo(parent.bottom)
-//                    centerTo(parent)
-//                }
-        )
+
         Box(
             modifier = Modifier
                 .size(40.dp)
                 .background(Color.Yellow)
-                .layoutId("yellowBox")
-//                .constrainAs(yellowBox) {
-//                    start.linkTo(magentaBox.end)
-//                    top.linkTo(magentaBox.bottom)
-//                }
+                .constrainAs(yellowBox) {
+                    start.linkTo(parent.start, margin = 40.dp)
+                }
         )
+
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Magenta)
+                .constrainAs(magentaBox) {
+                    start.linkTo(parent.start, margin = 60.dp)
+                }
+        )
+
+        createVerticalChain(redBox, yellowBox, magentaBox, chainStyle = ChainStyle.SpreadInside)
+        
+        val barrier = createEndBarrier(redBox, yellowBox, magentaBox)
+
+        Text(
+            text = "Test",
+            modifier = Modifier.constrainAs(text) {
+                start.linkTo(barrier)
+            }
+        )
+
     }
 }
 
