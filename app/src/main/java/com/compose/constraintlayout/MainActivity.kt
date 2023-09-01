3ef2599c5726b0d889b6ee141e5ddfcc59d99d63
송현서 <s22026@gsm.hs.kr>
2023-09-01 23:11:40 +0900

    Constraintlayout Example

--- app/src/main/java/com/compose/constraintlayout/MainActivity.kt
+++ app/src/main/java/com/compose/constraintlayout/MainActivity.kt
@@ -5,75 +5,94 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+import coil.compose.AsyncImage
 import com.compose.constraintlayout.ui.theme.ConstraintlayoutTheme
+import kotlinx.coroutines.NonDisposableHandle.parent
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ConstraintlayoutTheme {
-                ConstraintlayoutExample()
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    ConstraintlayoutExample(cardData)
+                    ConstraintlayoutExample(cardData)
+                    ConstraintlayoutExample(cardData)
+                }
             }
         }
     }
 }
 
 @Composable
-fun ConstraintlayoutExample() {
-    ConstraintLayout(Modifier.fillMaxSize()) {
-        val (redBox, yellowBox, magentaBox, text) = createRefs()
+fun ConstraintlayoutExample(cardData: CardData) {
+    val placeHolderColor = Color(0x33000000)
 
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(Color.Red)
-                .constrainAs(redBox) {
-                    start.linkTo(parent.start, margin = 10.dp)
+    Card(
+        modifier = Modifier.padding(4.dp),
+    ) {
+        // 단계 1: 아래의 Row 레이아웃을 ConstraintLayout로 바꾸어 봅시다.
+        ConstraintLayout(modifier = Modifier.fillMaxWidth()) {
+            val (profileImage, author, description) = createRefs()
+            AsyncImage(
+                model = cardData.imageUri,
+                contentDescription = cardData.imageDescription,
+                contentScale = ContentScale.Crop,
+                placeholder = ColorPainter(color = placeHolderColor),
+                modifier = Modifier
+                    .clip(CircleShape)
+                    .size(40.dp)
+                    .constrainAs(profileImage) {
+                        start.linkTo(parent.start, margin = 8.dp)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                        width = Dimension.value(40.dp)
+                    }
+            )
+
+            Text(
+                text = cardData.author,
+                modifier = Modifier.constrainAs(author) {
+                    linkTo(profileImage.end, parent.end, 8.dp, 8.dp, bias = 0f)
                 }
-        )
-
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(Color.Yellow)
-                .constrainAs(yellowBox) {
-                    start.linkTo(parent.start, margin = 40.dp)
+            )
+            Text(
+                text = cardData.description,
+                modifier = Modifier.constrainAs(description) {
+                    linkTo(profileImage.end, parent.end, 8.dp, 8.dp, bias = 0f)
+                    width = Dimension.fillToConstraints
                 }
-        )
+            )
 
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(Color.Magenta)
-                .constrainAs(magentaBox) {
-                    start.linkTo(parent.start, margin = 60.dp)
-                }
-        )
+            val chain = createVerticalChain(author, description)
 
-        createVerticalChain(redBox, yellowBox, magentaBox, chainStyle = ChainStyle.SpreadInside)
-        
-        val barrier = createEndBarrier(redBox, yellowBox, magentaBox)
-
-        Text(
-            text = "Test",
-            modifier = Modifier.constrainAs(text) {
-                start.linkTo(barrier)
+            constrain(chain) {
+                top.linkTo(parent.top, margin = 8.dp)
+                bottom.linkTo(parent.bottom, margin = 8.dp)
             }
-        )
-
+        }
     }
 }
 
@@ -81,6 +100,20 @@ fun ConstraintlayoutExample() {
 @Composable
 fun GreetingPreview() {
     ConstraintlayoutTheme {
-        ConstraintlayoutExample()
+        ConstraintlayoutExample(cardData)
     }
 }
+
+data class CardData(
+    val imageUri: String,
+    val imageDescription: String,
+    val author: String,
+    val description: String
+)
+
+val cardData = CardData(
+    imageUri = "https://raw.githubusercontent.com/Fastcampus-Android-Lecture-Project-2023/part4-chapter3/main/part4-chapter3-10/app/src/main/res/drawable-xhdpi/wall.jpg",
+    imageDescription = "엔텔로프 캐년",
+    author = "Dalinaum",
+    description = "엔텔로프 캐년은 죽기 전에 꼭 봐야할 절경으로 소개되었습니다."
+)
