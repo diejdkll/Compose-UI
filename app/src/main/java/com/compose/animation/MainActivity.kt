120523fe1dea52385a8a84ea18afc488134c69a0
송현서 <s22026@gsm.hs.kr>
2023-09-18 12:27:27 +0900

    Animation1

---
+++ app/src/main/java/com/compose/animation/MainActivity.kt
@@ -0,0 +1,159 @@
+package com.compose.animation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.expandIn
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.slideOutHorizontally
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.compose.animation.ui.theme.AnimationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AnimationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AnimationExample()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AnimationExample() {
+    var helloWorldVisible by remember {
+        mutableStateOf(true)
+    }
+    var isRed by remember {
+        mutableStateOf(false)
+    }
+
+    val backgroundColor by animateColorAsState(
+        targetValue = if (isRed) Color.Red else Color.White, label = ""
+    )
+
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .background(backgroundColor)
+    ) {
+        AnimatedVisibility(
+            visible = helloWorldVisible,
+            enter = expandHorizontally() + fadeIn(),
+            exit = slideOutHorizontally()
+        ) {
+            Text(text = "Hello World!")
+        }
+
+        Row(
+            Modifier.selectable(
+                selected = helloWorldVisible,
+                onClick = {
+                    helloWorldVisible = true
+                }
+            ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = helloWorldVisible,
+                onClick = { helloWorldVisible = true }
+            )
+            Text(
+                text = "Hello World 보이기"
+            )
+        }
+
+        Row(
+            Modifier.selectable(
+                selected = !helloWorldVisible,
+                onClick = {
+                    helloWorldVisible = false
+                }
+            ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = !helloWorldVisible,
+                onClick = { helloWorldVisible = false }
+            )
+            Text(
+                text = "Hello World 감추기"
+            )
+        }
+
+        Text(text = "배경 색을 바꾸어봅시다.")
+
+        Row(
+            Modifier.selectable(
+                selected = !isRed,
+                onClick = {
+                    isRed = false
+                }
+            ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = !isRed,
+                onClick = { isRed = false }
+            )
+            Text(
+                text = "흰색"
+            )
+        }
+
+        Row(
+            Modifier.selectable(
+                selected = isRed,
+                onClick = {
+                    isRed = true
+                }
+            ),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            RadioButton(
+                selected = isRed,
+                onClick = { isRed = true }
+            )
+            Text(
+                text = "빨간색"
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AnimationTheme {
+        AnimationExample()
+    }
+}
