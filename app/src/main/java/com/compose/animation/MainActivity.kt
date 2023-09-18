897e1b8e8001c2e65277f536433e3c46edabb045
송현서 <s22026@gsm.hs.kr>
2023-09-18 16:09:34 +0900

    Animation2

--- app/src/main/java/com/compose/animation/MainActivity.kt
+++ app/src/main/java/com/compose/animation/MainActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.Crossfade
+import androidx.compose.animation.animateColor
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.animation.expandHorizontally
-import androidx.compose.animation.expandIn
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.slideOutHorizontally
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
@@ -26,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -53,100 +59,148 @@ fun AnimationExample() {
     var helloWorldVisible by remember {
         mutableStateOf(true)
     }
-    var isRed by remember {
+    var isDarkMode by remember {
         mutableStateOf(false)
     }
 
-    val backgroundColor by animateColorAsState(
-        targetValue = if (isRed) Color.Red else Color.White, label = ""
-    )
+    val transition = updateTransition(targetState = isDarkMode, label = "다크 모드 트랜지션")
+
+    val backgroundColor by transition.animateColor(label = "다크 모드 배경색상 애니메이션") { state ->
+        when(state) {
+            false -> Color.White
+            true -> Color.Black
+        }
+    }
+
+    val color by transition.animateColor(label = "다크 모드 글자색상 애니메이션") { state ->
+        when(state) {
+            false -> Color.Black
+            true -> Color.White
+        }
+    }
+
+    val alpha by transition.animateFloat(label = "다크 모드 알파 애니메이션") { state ->
+        when(state) {
+            false -> 0.7f
+            true -> 1f
+        }
+    }
 
     Column(
         modifier = Modifier
             .padding(16.dp)
             .background(backgroundColor)
+            .alpha(alpha)
     ) {
         AnimatedVisibility(
             visible = helloWorldVisible,
             enter = expandHorizontally() + fadeIn(),
             exit = slideOutHorizontally()
         ) {
-            Text(text = "Hello World!")
-        }
-
-        Row(
-            Modifier.selectable(
-                selected = helloWorldVisible,
-                onClick = {
-                    helloWorldVisible = true
-                }
-            ),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            RadioButton(
-                selected = helloWorldVisible,
-                onClick = { helloWorldVisible = true }
-            )
             Text(
-                text = "Hello World 보이기"
+                text = "Hello World!",
+                color = color
             )
         }
 
-        Row(
-            Modifier.selectable(
-                selected = !helloWorldVisible,
-                onClick = {
-                    helloWorldVisible = false
-                }
-            ),
-            verticalAlignment = Alignment.CenterVertically
+        RadioButtonWithText(
+            text = "Hello World 보이기",
+            color = color,
+            selected = helloWorldVisible,
         ) {
-            RadioButton(
-                selected = !helloWorldVisible,
-                onClick = { helloWorldVisible = false }
-            )
-            Text(
-                text = "Hello World 감추기"
-            )
+            helloWorldVisible = true
         }
 
-        Text(text = "배경 색을 바꾸어봅시다.")
-
-        Row(
-            Modifier.selectable(
-                selected = !isRed,
-                onClick = {
-                    isRed = false
-                }
-            ),
-            verticalAlignment = Alignment.CenterVertically
+        RadioButtonWithText(
+            text = "Hello World 숨기기",
+            color = color,
+            selected = !helloWorldVisible,
         ) {
-            RadioButton(
-                selected = !isRed,
-                onClick = { isRed = false }
-            )
-            Text(
-                text = "흰색"
-            )
+            helloWorldVisible = false
         }
 
-        Row(
-            Modifier.selectable(
-                selected = isRed,
-                onClick = {
-                    isRed = true
-                }
-            ),
-            verticalAlignment = Alignment.CenterVertically
+        Text(
+            text = "배경 색을 바꾸어봅시다.",
+            color = color
+        )
+
+        RadioButtonWithText(
+            text = "일반 모드",
+            color = color,
+            selected = !isDarkMode,
         ) {
-            RadioButton(
-                selected = isRed,
-                onClick = { isRed = true }
-            )
-            Text(
-                text = "빨간색"
-            )
+            isDarkMode = false
         }
+
+        RadioButtonWithText(
+            text = "다크 모드",
+            color = color,
+            selected = isDarkMode,
+        ) {
+            isDarkMode = true
+        }
+
+        Crossfade(targetState = isDarkMode, label = "") { state ->
+            when(state) {
+                false -> {
+                    Row {
+                        Box(modifier = Modifier
+                            .background(Color.Red)
+                            .size(20.dp)) {
+                            Text("1")
+                        }
+                        Box(modifier = Modifier
+                            .background(Color.Magenta)
+                            .size(20.dp)) {
+                            Text("2")
+                        }
+                        Box(modifier = Modifier
+                            .background(Color.Blue)
+                            .size(20.dp)) {
+                            Text("3")
+                        }
+                    }
+                }
+                true -> {
+                    Column {
+                        Box(modifier = Modifier
+                            .background(Color.Red)
+                            .size(20.dp)) {
+                            Text("A")
+                        }
+                        Box(modifier = Modifier
+                            .background(Color.Magenta)
+                            .size(20.dp)) {
+                            Text("B")
+                        }
+                        Box(modifier = Modifier
+                            .background(Color.Blue)
+                            .size(20.dp)) {
+                            Text("C")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RadioButtonWithText(
+    text: String,
+    color: Color = Color.Black,
+    selected: Boolean,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier.selectable(
+            selected = selected,
+            onClick = onClick
+        ),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(selected = selected, onClick = onClick)
+        Text(text = text, color = color)
     }
 }
 
